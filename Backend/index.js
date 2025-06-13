16132130d325a775c41dd001f45b0b170dc519bd
vikasco987 <vikasvikas9095@gmail.com>
2025-06-13 17:05:32 +0530

    GIT ADD.

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import planRoutes from "./routes/plan.js";
-
-// dotenv.config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/plan", planRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// mongoose
-//   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//   })
-//   .catch((err) => console.log(err));
-
 
 import express from "express";
 import mongoose from "mongoose";
@@ -33,19 +11,19 @@ import cashfreeRoutes from './routes/cashfreeRoutes.js';
 dotenv.config();
 
 const app = express();
- app.use(cors());
-// const allowedOrigins = ["http://localhost:5173", "https://yourfrontend.com"];
 
-// // app.use(cors({
-// //   origin: function (origin, callback) {
-// //     if (!origin || allowedOrigins.includes(origin)) {
-// //       callback(null, true);
-// //     } else {
-// //       callback(new Error("Not allowed by CORS"));
-// //     }
-// //   },
-//   credentials: true,
-// }));
+const allowedOrigins = ["http://localhost:5173", "https://magicscale.in"];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
 
