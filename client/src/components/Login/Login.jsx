eb4299e381544ecab05409828c37668336ca2ef1
vikasco987 <vikasvikas9095@gmail.com>
2025-06-24 17:03:39 +0530

    add

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -20,7 +20,8 @@ const Login = () => {
     setError('');
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      //'http://localhost:5000/api/auth/login'
+      const res = await fetch('https://magicscale-backend.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
