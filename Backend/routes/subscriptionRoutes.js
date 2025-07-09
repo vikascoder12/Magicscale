3ac685e508d80a44e9c1cc605b691844d2547ff7
vikasco987 <vikasvikas9095@gmail.com>
2025-07-10 02:14:57 +0530

    add

--- Backend/routes/subscriptionRoutes.js
+++ Backend/routes/subscriptionRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 // n
-import { getUserSubscriptions } from '../controllers/userController.js';
+//import { getUserSubscriptions } from '../controllers/userController.js';
 
 const router = express.Router();
 
